7b5cfc7bbdec9fb69617615e5c04fc8eab8a216e
Runze <sendrunzedong@gmail.com>
2016-04-22 19:15:21 -0400

    updating 04/2016

--- Stringprocess/pointers/commandecho.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-//echo command-line arguments. 
-int main(int argc, char *argv[]){
-	int i;
-	for(i=1;i<argc;i++)
-		printf("%s%s\n", argv[i],(i<argc-1)?" ":"");
-	printf("\n");
-	return 0;
-}
--- Stringprocess/pointers/echo2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-main(int argc, char *argv[]){
-	while(--argc>0)
-		printf("%s%s",*++argv,(argc>1)?" ":"");
-	printf("\n");
-	return 0;
-}
